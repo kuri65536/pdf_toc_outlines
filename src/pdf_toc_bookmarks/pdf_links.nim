adbe847d05008c416467cc3a8aa0a6f4f8636ad4
shimoda <kuri65536@hotmail.com>
2024-11-07 09:04:17 +0900

    fitz: extract contents.

--- src/pdf_toc_bookmarks/pdf_links.nim
+++ src/pdf_toc_bookmarks/pdf_links.nim
@@ -11,11 +11,34 @@ type
   PdfLink* = ref object of RootObj
     p_link: pointer
     uri*: string
+    title*: string
+    x*: float
+    n_page: int
 
 
 proc fz_load_links(a, b: pointer, n_page: int): pointer =
     {.emit: """fz_page* page = fz_load_page(`a`, `b`, `n_page`);
                `result` = fz_load_links(`a`, page);
+               """}
+
+
+proc fz_load_text(a, b: pointer, n_page: int): pointer =
+    proc fn(ctx, state, data: pointer, n: uint): void {.cdecl.} =
+        let tmp = cast[ptr UncheckedArray[char]](data)
+        var s: string
+        for i in 0 .. n - 1:
+            let ch = tmp[i]
+            if ch == '\n' or len(s) > 80:
+                echo(s)
+                s = ""
+            else:
+                s = s & ch
+        if len(s) > 0:
+            echo(s)
+    {.emit: """ fz_page* page = fz_load_page(`a`, `b`, `n_page`);
+                fz_output* out = fz_new_output(`a`, 1024, NULL, `fn`, NULL, NULL);
+                fz_device* dev = fz_new_xmltext_device(`a`, out);
+                fz_run_page(`a`, page, dev, fz_identity, NULL);
                """}
 
 
@@ -30,6 +53,8 @@ proc pdf_link*(pdf: PdfDoc, n_page: int): PdfLink =
                `tmp` = l->uri;
                """}
     result.uri = $tmp
+    result.n_page = n_page
+    discard fz_load_text(pdf.fitz, pdf.fitz_doc, n_page)
 
 
 proc pdf_link_next*(link: PdfLink): PdfLink =
@@ -46,6 +71,7 @@ proc pdf_link_next*(link: PdfLink): PdfLink =
     {.emit: """fz_link* l2 = (fz_link*)`p`;
                `tmp` = l2->uri;
                """}
-    result = PdfLink(p_link: p,
+    result = PdfLink(n_page: link.n_page,
+                     p_link: p,
                      uri: $tmp)
 
