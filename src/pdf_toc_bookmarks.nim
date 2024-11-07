a8d9deb1cdbeb855fbdedb0a73d97946681226dd
shimoda <kuri65536@hotmail.com>
2024-11-07 09:03:23 +0900

    app: add skelton code of the nim command line tool.

--- src/pdf_toc_bookmarks.nim
+++ src/pdf_toc_bookmarks.nim
@@ -1,5 +1,33 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+##[pdf_toc_bookmarks.nim
+
+License: MIT, see LICENSE
+]##
+import os
+import system
+
+
+type
+  Options* = ref object of RootObj
+    n_quit: int
+
+
+proc options(args: seq[string]): Options =
+    var ret = Options(
+                      )
+    return ret
+
+
+proc main(args: seq[string]): int =
+    let opts = options(args)
+    if opts.n_quit != 0:
+        return opts.n_quit
+    ## @todo impl run
+    return 0
+
 
 when isMainModule:
-  echo("Hello, World!")
+    var args: seq[string] = @[]
+    for i in 1..os.paramCount():
+        args.add(os.paramStr(i))
+    system.quit(main(args))
+
